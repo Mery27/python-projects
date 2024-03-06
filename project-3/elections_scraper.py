280eff629b6d4e5dde38810efc80984e1a622c66
Jiří Merenda <j.merenda@seznam.cz>
2024-03-06 12:05:07 +0100

    Update, work for other world

--- project-3/elections_scraper.py
+++ project-3/elections_scraper.py
@@ -17,19 +17,30 @@ import time
 
 
 base_url = "https://volby.cz/pls/ps2017nss"
+world = "ps36?xjazyk=CZ"
 request_timeout_seconds = 120
+title_code = "code"
+title_location = "location"
+title_continent = "continent"
+title_countries = "countries"
+title_city = "city"
+title_count_district = "count_district"
+title_registred = "registred"
+title_envelops = "envelops"
+title_valid = "valid"
+title_election_results = "election_results"
+title_election_candidates = "election_candidates"
+title_url_link = "url"
 # list of election candidates will be added on the end of the header
-header = ["code", "location", "registred", "envelops", "valid"]
-election_results = "election_results"
-election_candidates = "election_candidates"
-url_link = "url"
+header = [title_code, title_location, title_registred, title_envelops, title_valid]
+header_countries = [title_continent, title_countries, title_city, title_registred, title_envelops, title_valid] 
 
 
 def create_url(base_url: str, join_url: str) -> str:
     return base_url + "/" + join_url
 
 
-def get_response(url: str) -> requests.models.Response|bool:
+def get_response(url: str) -> requests.models.Response|None:
     '''
     Try connection to the url, if it fails in any case, program will be stopped.
     '''
@@ -51,14 +62,16 @@ def get_response(url: str) -> requests.models.Response|bool:
         return response
 
 
-def get_content_from_url(response: requests.models.Response) -> bs:
+def get_content_from_url(website_url: str) -> bs:
     '''
     Return html content from url.
     '''
+    response = get_response(create_url(base_url, website_url))
+
     return bs(response.content, features="html.parser")
 
 
-def get_data_select_village(url: str) -> dict:
+def get_data_select_village(website_url: str) -> dict:
     '''
     Get dict with all cities for select.
 
@@ -71,7 +84,7 @@ def get_data_select_village(url: str) -> dict:
                 },\n
             'Benešov': {....\n
     '''
-    html_page = get_content_from_url(get_response(create_url(base_url, url)))
+    html_page = get_content_from_url(website_url)
     rows = html_page.select("table tr:nth-child(n+3)")
     
     dict_of_results = {}
@@ -82,22 +95,22 @@ def get_data_select_village(url: str) -> dict:
             url = row.select("td:last-child a")[0].attrs["href"]
 
             dict_of_results[title] = {
-                header[1]: title,
-                header[0]: code,
-                url_link: url,
-                election_results : {} }
+                title_location: title,
+                title_code: code,
+                title_url_link: url,
+                title_election_results : {} }
 
     return dict_of_results
 
 
-def get_data_select_district(url: str) -> dict:
+def get_data_select_district(website_url: str) -> dict:
     '''
     Get result from table, only for "vyber okrsku"
 
     Result:
         {"code": "url"}
     '''
-    html_page: bs = get_content_from_url(get_response(create_url(base_url, url)))
+    html_page: bs = get_content_from_url(website_url)
     rows = html_page.select("table tr:nth-child(n+2)")
     
     dict_of_results = {}
@@ -111,12 +124,53 @@ def get_data_select_district(url: str) -> dict:
     return dict_of_results
 
 
-def get_result_election(url: str) -> dict:
+def get_international_village(website_url: str) -> dict:
+    '''
+    Get dict with all countries for select.
+
+    Return:
+        {0: 
+            {
+                'election_results': {},
+                'city': 'Tirana',
+                'count_district': '1',
+                'continent': 'Evropa',
+                'url': 'ps311?xjazyk=CZ&xkraj=2&xobec=999997&xsvetadil=EV&xzeme=8&xokrsek=1',
+                'countries': 'Albánie'
+            },
+    '''
+    html_page = get_content_from_url(website_url)
+    rows = html_page.select("table tr:nth-child(n+2)")
+    
+    current_continent = ""
+    current_countries = ""
+    dict_of_results = {}
+    for index, row in enumerate(rows):
+        if row.select_one("td[headers='s1'] a"):
+            current_continent = row.select_one("td[headers='s1'] a").get_text().replace("\n", "")
+        if row.select_one("td[headers='s2']"):
+            current_countries = row.select_one("td[headers='s2']").get_text().replace("\n", "")
+        city = row.select_one("td[headers='s3']").get_text()
+        count_district = row.select_one("td[headers='s4']").get_text().replace("\n","")
+        url = row.select_one("td:last-child a").attrs["href"]
+
+        dict_of_results[index] = {
+            title_continent: current_continent,
+            title_countries: current_countries,
+            title_city: city,
+            title_count_district: count_district,
+            title_url_link: url,
+            title_election_results : {} }
+
+    return dict_of_results
+
+
+def get_result_election(website_url: str) -> dict:
     '''
     Get result as registred, envelops, valid and all election candidates
 
     Result example:
-        {'election_candidate': {
+        {'election_candidates': {
             'ANO 2011': '32',
             'Blok proti islam.-Obran.domova': '0',
             ...and others
@@ -126,16 +180,20 @@ def get_result_election(url: str) -> dict:
         'valid': '144'
         }
     '''
-    html_page: bs = get_content_from_url(get_response(create_url(base_url, url)))
+    html_page: bs = get_content_from_url(website_url)
     table_sum_election = html_page.select("#publikace > table tr:last-child")
 
-    result_sum_election = { header[2]: 0, header[3]: 0, header[4]: 0, election_candidates : {} }
+    result_sum_election = {title_registred: 0,
+                            title_envelops: 0,
+                            title_valid: 0,
+                            title_election_candidates : {} }
+    
     for col in table_sum_election:
-        result_sum_election[header[2]] = int(col.select(
+        result_sum_election[title_registred] = int(col.select(
             "td[headers='sa2']")[0].get_text().replace("\xa0", ""))
-        result_sum_election[header[3]] = int(col.select(
+        result_sum_election[title_envelops] = int(col.select(
             "td[headers='sa3']")[0].get_text().replace("\xa0", ""))
-        result_sum_election[header[4]] = int(col.select(
+        result_sum_election[title_valid] = int(col.select(
             "td[headers='sa6']")[0].get_text().replace("\xa0", ""))
     
     table_election_candidate = html_page.select("#outer table tr:nth-child(n+3)")
@@ -143,7 +201,7 @@ def get_result_election(url: str) -> dict:
         if row.select("td:nth-child(1)")[0].get_text().isnumeric():
             title = row.select("td:nth-child(2)")[0].get_text()
             votes = row.select("td:nth-child(3)")[0].get_text().replace("\xa0", "")
-            result_sum_election[election_candidates][title] = int(votes)
+            result_sum_election[title_election_candidates][title] = int(votes)
 
     return result_sum_election
 
@@ -179,7 +237,7 @@ if len(sys.argv) != 3:
 
 user_url = sys.argv[1].split("/")[-1]
 
-if user_url.find("kraj") == -1 or user_url.find("xnumnuts") == -1:
+if user_url.find("xnumnuts") == -1 and not user_url == world:
     separator_line()
     print("You enter wrong format URL adress, missing some paramater 'kraj=' or 'xnumnuts='.")
     quit()
@@ -191,72 +249,68 @@ separator_line()
 print("Download data from url: ", user_url)
 separator_line()
 
-# initialization variable for all villages with election data
 result_villages = {}
 start_time = time.time()
 # list of all villages with url adress which we need to process
-list_villages = get_data_select_village(user_url).items()
-# complete region result from all villages and distrcit into result_villages variable
+if user_url == world:
+    list_villages = get_international_village(user_url).items()
+    header = header_countries
+else:
+    list_villages = get_data_select_village(user_url).items()
+# COMPLETE RESULT FROM ALL VILLAGES(CITIES) AND DISTRICT into result_villages variable
 for index_village, (title, village) in enumerate(list_villages):
     result_villages[title] = village
-    village_url = village[url_link]
-    # if url parameter "vyber" is in url you get election result page
-    if village_url.find("vyber") > 1:
-        result_villages[title][election_results] = get_result_election(village_url)
+    village_url = village[title_url_link]
+    # if url parameter "vyber" or "svetadil" is in url you get direct election result page
+    if village_url.find("vyber") >= 0 or village_url.find("svetadil") >= 0:
+        result_villages[title][title_election_results] = get_result_election(village_url)
         progress_bar(index_village, len(list_villages), 0, 0, start_time)
     else:
         list_of_districts = get_data_select_district(village_url).values()
         for index_disctrict, district_url in enumerate(list_of_districts):
             result_for_disctrict = get_result_election(district_url)
             if index_disctrict == 0:
-                result_villages[title][election_results] = result_for_disctrict
+                result_villages[title][title_election_results] = result_for_disctrict
             else:
                 for key, value in result_for_disctrict.items():
-                    if key == election_candidates:
+                    if key == title_election_candidates:
                         for candidate in result_for_disctrict[key]:
-                            result_villages[title][election_results][election_candidates][candidate] += (
-                                int(result_for_disctrict[election_candidates][candidate]))
+                            result_villages[title][title_election_results][title_election_candidates][candidate] += (
+                                int(result_for_disctrict[title_election_candidates][candidate]))
                     else:
-                        result_villages[title][election_results][key] += int(result_for_disctrict[key])
+                        result_villages[title][title_election_results][key] += int(result_for_disctrict[key])
             progress_bar(index_village, len(list_villages), index_disctrict,
                          len(list_of_districts), start_time)
 
-
-first_result_election_candidates = next(iter(result_villages.values()))[election_results]
-all_election_candidates = first_result_election_candidates.get(election_candidates).keys()
+# Exted header with all candidates which we get from first item in result result_villages
+first_result_election_candidates = next(iter(result_villages.values()))[title_election_results]
+all_election_candidates = first_result_election_candidates.get(title_election_candidates).keys()
 header.extend(all_election_candidates)
 
 separator_line(space_top=True)
 print("Saving data to te file: ", user_file)
 
 result = []
+# CREATE ROWS FOR CSV FILE
 for index, village in enumerate(result_villages.values()):
     # Progres bar
     print(f"\rProgress: {index + 1}/{len(result_villages)}", end='', flush=True)
     # fill row with numbers for length of header
     row = [i for i in range(len(header))]
-    for key_1_lvl in village:
-        # we dont need url in result
-        if key_1_lvl == url_link:
-            continue
+    for key_1_lvl, value_1_lvl in village.items():
         # if key in header, write to header in same index
         if key_1_lvl in header:
             row[header.index(key_1_lvl)] = village.get(key_1_lvl)
-        elif not village.get(key_1_lvl, {}):
-            # dont loop throw empy dict
-            continue
-        else:
-            for key_2_lvl in village.get(key_1_lvl):
+        if key_1_lvl not in header and isinstance(value_1_lvl, dict):
+            for key_2_lvl, value_2_lvl in village.get(key_1_lvl).items():
                 if key_2_lvl in header:
                     row[header.index(key_2_lvl)] = village.get(
                         key_1_lvl, {}).get(key_2_lvl, {})
-                elif not village.get(key_1_lvl, {}).get(key_2_lvl, {}):
-                    continue
-                else:
-                    for key_3_lvl in village.get(key_1_lvl).get(key_2_lvl):
-                        row[header.index(key_3_lvl)] = village.get(
-                            key_1_lvl, {}).get(key_2_lvl, {}).get(key_3_lvl, {})
-
+                if key_2_lvl not in header and isinstance(value_2_lvl, dict):
+                    for key_3_lvl, value_3_lvl in village.get(key_1_lvl).get(key_2_lvl).items():
+                        if key_3_lvl in header:
+                            row[header.index(key_3_lvl)] = village.get(
+                                key_1_lvl, {}).get(key_2_lvl, {}).get(key_3_lvl, {})
     result.append(row)
 
 
